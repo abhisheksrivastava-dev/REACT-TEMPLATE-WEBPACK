757d1e0b383315471add9cf620e59466595f489c
Abhishek Srivastava <74584768+abhisheksrivastava-dev@users.noreply.github.com>
2022-12-23 15:17:46 +0530

    add prettier

--- src/ClickCounter.tsx
+++ src/ClickCounter.tsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react'
 
 export const ClickCounter = () => {
-  const [count, setCount] = React.useState(0);
+  const [count, setCount] = React.useState(0)
   return (
     <div>
       <button onClick={() => setCount((previousCount) => previousCount + 1)}>
         Count {count}
       </button>
     </div>
-  );
-};
+  )
+}
