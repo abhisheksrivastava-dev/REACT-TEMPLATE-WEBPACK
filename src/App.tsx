741a555d46edbefcbb32cf8890b3ec9e31d3c481
Abhishek Srivastava <74584768+abhisheksrivastava-dev@users.noreply.github.com>
2022-12-23 14:52:11 +0530

    add env & react refresh module

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import "./styles.css";
 import IMAGE from "./react.png";
 import LOGO from "./logo.svg";
+import { ClickCounter } from "./ClickCounter";
 
 export const App = () => {
   return (
     <>
-      <h1>React TypeScript Webpack Starter Template</h1>
+      <h1>Hello React TypeScript Webpack Starter Template - {process.env.NODE_ENV} - {process.env.name}</h1>
       <img src={IMAGE} alt="React Logo" width="300" height="200" />
       <img src={LOGO} alt="React Logo" width="300" />
+      <ClickCounter />
     </>
   );
 };
